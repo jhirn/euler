35f350ee62b37d5d8e591fcf33bc06becac7cdc5
Joe Hirn <joseph.hirn@gmail.com>
2011-03-10 21:49:28 -0600

    added clj-http, enlive, swank deps

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject euler "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[speclj "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-http "0.1.3"]
+                 [enlive "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[speclj "1.2.0"]
+                     [swank-clojure "1.2.1"]]
   :test-path "spec/"
   )
