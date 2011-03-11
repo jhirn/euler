ac51fd6f512f2468aa97d1be07153eb2df8b9c80
Joe Hirn <joseph.hirn@gmail.com>
2011-03-11 11:24:52 -0600

    deleted some test structures from the scraper

--- src/leiningen/problem-scraper.clj
+++ src/leiningen/problem-scraper.clj
@@ -13,14 +13,10 @@
   (let [html (-> (problem-url problem-number) reader html-resource)]
     (select html [:div.problem_content])))
 
-(def element {:tag :div,  :content '("2" "3" {:tag :p, :content ("4")})})
-
-(def sup-tag  {:tag :sup, :content '("2")})
-
-(def p-content '({:tag :p, :content '("hi")}))
 
 (def decorators {:p     #(cons "\n" %1)
                  :sup   #(cons "^" %1)})
+
 (defn decorate [content tag]
   (let [ decorator (tag decorators)]
     (if (nil? decorator)
