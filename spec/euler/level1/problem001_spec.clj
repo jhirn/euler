11d944173f46efb6945e2472cd7b91af3ec29f11
Joe Hirn <joseph.hirn@gmail.com>
2011-03-11 02:17:20 -0600

    problem 1 working yay

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -1,15 +1,26 @@
 (ns euler.level1.problem001-spec
   (:use
-    [speclj.core]
-    [euler.level1.problem001]))
+   [speclj.core]
+   [euler.level1.problem001]))
 
-; http://projecteuler.net/index.php?section=problems&id=1
+                                        ; http://projecteuler.net/index.php?section=problems&id=1
+
+
+(describe "multiple-of?"
+          (it "should return true if divisor is a multiple of n"
+              (should (multiple-of? 10 5)))
+          (it "should return false if divisior is not a multiple of n"
+              (should-not (multiple-of? 10 3))))
+
+(describe "sum"
+          (it "should sum numbers"
+              (should= 6 (sum '(1 2 3)))))
 
 (describe "Euler Problem #1"
 
-  (it "Solves #1"
-    (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
-  )
+          (it "Solves #1"
+              (should= 23 (euler-1 10)))
+          )
+
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,16 @@
 (ns euler.level1.problem001)
 
+;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(defn multiple-of? [n divisor]
+  (= (rem n divisor)0))
+
+(defn sum [s] (apply + s))
+
 (defn euler-1 [n]
-  )
+ (sum (filter #(or
+            (multiple-of? % 3)
+            (multiple-of? % 5)) (range 1 n))))
+
